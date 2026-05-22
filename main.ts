6845a554d126df07d597e935fba63535ea18ea5f
woo2324078-lgtm <woo2324078@dcfwms.edu.hk>
2026-05-22 09:13:30 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 input.onButtonPressed(Button.A, function () {
-    music.play(music.stringPlayable("G E G - G E G - ", 120), music.PlaybackMode.UntilDone)
-    music.play(music.stringPlayable("A G F E D E F - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("C C G G A A G - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("F F E E D D C - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("G G F F E E D - ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("G G F F E E D - ", 120), music.PlaybackMode.UntilDone)
 })
 basic.showNumber(67)
 basic.showString("ROBLOX")
